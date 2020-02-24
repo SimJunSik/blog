e8967f2ef60a708957af4dc545b799f0a53cecfc
SimJunSik <wnstlr24@naver.com>
2020-02-24 20:40:30 +0900

    backend/api/posts 추가

---
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -0,0 +1,150 @@
+const Post = require('../../models/post');
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const { ObjectId } = mongoose.Types;
+
+exports.checkObjectId = (ctx, next) => {
+  const { id } = ctx.params;
+  if (!ObjectId.isValid(id)) {
+    ctx.status = 400;
+    return;
+  }
+  return next();
+};
+
+/*
+    POST /api/posts
+    {
+        title: '제목',
+        body: '내용',
+        tags: ['태그1', '태그2']
+    }
+*/
+exports.write = async ctx => {
+  const schema = Joi.object().keys({
+    title: Joi.string().required(),
+    body: Joi.string().required(),
+    tags: Joi.array()
+      .items(Joi.string())
+      .required(),
+  });
+  const result = Joi.validate(ctx.request.body, schema);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
+  const { title, body, tags } = ctx.request.body;
+  const post = new Post({
+    title,
+    body,
+    tags,
+  });
+  try {
+    await post.save();
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+/*
+    GET /api/posts
+*/
+exports.list = async ctx => {
+  const page = parseInt(ctx.query.page || '1', 10);
+  if (page < 1) {
+    ctx.status = 400;
+    return;
+  }
+
+  try {
+    const posts = await Post.find()
+      .sort({ _id: -1 })
+      .limit(10)
+      .skip((page - 1) * 10)
+      .exec();
+    const postCount = await Post.countDocuments().exec();
+    // 마지막 페이지값을 나타낼 Header 추가
+    ctx.set('Last-Page', Math.ceil(postCount / 10));
+    // body가 200자 넘으면 ...으로 response
+    ctx.body = posts
+      .map(post => post.toJSON())
+      .map(post => ({
+        ...post,
+        body:
+          post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,
+      }));
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+/*
+    GET /api/posts/:id
+*/
+exports.read = async ctx => {
+  const { id } = ctx.params;
+  try {
+    const post = await Post.findById(id).exec();
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+/*
+    DELETE /api/posts/:id
+*/
+exports.remove = async ctx => {
+  const { id } = ctx.params;
+  try {
+    await Post.findByIdAndRemove(id).exec();
+    ctx.status = 204;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+/*
+    PATCH /api/posts/:id
+    {
+        title: '수정',
+        body: '수정 내용',
+        tags: ['수정', '태그']
+    }
+*/
+exports.update = ctx => {
+  const { id } = ctx.params;
+
+  const schema = Joi.object().keys({
+    title: Joi.string(),
+    body: Joi.string(),
+    tags: Joi.array().items(Joi.string()),
+  });
+  const result = Joi.validate(ctx.request.body, schema);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
+  try {
+    const post = Post.findByIdAndUpdate(id, ctx.request.body, {
+      new: true,
+    }).exec();
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
---
+++ blog-backend/src/index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
+
+const api = require('./api');
+
+const app = new Koa();
+const router = new Router();
+const { PORT, MONGO_URI } = process.env;
+
+// DB
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(e => {
+    console.error(e);
+  });
+
+// 라우터 설정
+router.use('/api', api.routes());
+
+// Middleware
+app.use(bodyParser());
+
+// 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
+
+const port = PORT || 4000;
+app.listen(port, () => console.log(`Listening to port ${port}`));
