9f03629b2430e0d21414f4c3355eef05064623fe
SimJunSik <wnstlr24@naver.com>
2020-02-25 08:28:47 +0900

    frontend/회원인증 UI 추가

--- blog-frontend/src/App.js
+++ blog-frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <Route component={PostListPage} path={['/@:username', '/']} exact />
-      <Route component={LoginPage} path="login" />
+      <Route component={LoginPage} path="/login" />
       <Route component={RegisterPage} path="/register" />
       <Route component={WritePage} path="/write" />
       <Route component={PostPage} path="/@:username/:postId" />
