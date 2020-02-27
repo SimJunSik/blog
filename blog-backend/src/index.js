1ab3f6321ac77d9e3437ce8ea8eb8544472165f8
SimJunSik <wnstlr24@naver.com>
2020-02-27 10:12:55 +0900

    backend/koa-static 추가

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -3,6 +3,9 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 const mongoose = require('mongoose');
+const serve = require('koa-static');
+const path = require('path');
+const send = require('koa-send');
 
 const api = require('./api');
 const jwtMiddleware = require('./lib/jwtMiddleware');
@@ -30,6 +33,16 @@ app.use(jwtMiddleware);
 
 // 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
+
+const buildDirectory = path.resolve(__dirname, '../../blog-frontend/build');
+app.use(serve(buildDirectory));
+app.use(async ctx => {
+  // Not Found이고, 주소가 /api로 시작하지 않는 경우
+  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+    // index.html 내용을 반환
+    await send(ctx, 'index.html', { root: buildDirectory });
+  }
+});
 
 const port = PORT || 4000;
 app.listen(port, () => console.log(`Listening to port ${port}`));
